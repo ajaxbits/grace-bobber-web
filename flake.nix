30a7c46fa0097acc6182f439fd43e0443daaf7f8
Alex Jackson <45179933+ajaxbits@users.noreply.github.com>
2021-12-14 14:53:16 -0600

    some fun building docker image

--- flake.nix
+++ flake.nix
@@ -9,27 +9,43 @@
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-	pkgs = nixpkgs.legacyPackages.${system};
-	nodeEnv = pkgs.callPackage ./default.nix { };
+        pkgs = nixpkgs.legacyPackages.${system};
+        nodeEnv = pkgs.callPackage ./default.nix { };
+        revision = "${self.lastModifiedDate}-${self.shortRev or "dirty"}";
       in rec {
+        # packages = {
+        #   ociImage = pkgs.dockerTools.buildLayeredImage {
+        #     name = "gracebobber.com";
+        #     contents = [ pkgs.python3Minimal ];
+        #     tag = revision;
+        #     config = {
+        #       Cmd = [
+        #         "${pkgs.python3Minimal}/bin/python"
+        #         "-m"
+        #         "http.server"
+        #         "8000"
+        #       ];
+        #     };
+        #   };
+        # };
+
         devShell = pkgs.mkShell {
-          buildInputs = with pkgs; with nodePackages; [
-	      nodejs
-	      node2nix
-	      nodeEnv.shell.nodeDependencies
-	      imagemagick
-	      libwebp
-	      jpegoptim
-	      optipng
-	      svgo
-	      fd
-	      exiftool
-	    ];
-	  shellHook = ''
-	    export NODE_PATH=${nodeEnv.shell.nodeDependencies}/lib/node_modules
-	    exiftool -title="Grace Bobber Resume" images/Grace-Bobber-Resume.pdf
-	  '';
-	};
-      }
-    );
+          buildInputs = with pkgs;
+            with nodePackages; [
+              nodejs
+              node2nix
+              nodeEnv.shell.nodeDependencies
+              imagemagick
+              libwebp
+              jpegoptim
+              optipng
+              svgo
+              fd
+              exiftool
+            ];
+          shellHook = ''
+            export NODE_PATH=${nodeEnv.shell.nodeDependencies}/lib/node_modules
+          '';
+        };
+      });
 }
