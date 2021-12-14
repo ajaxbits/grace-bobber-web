a7ca36769816ff9d1ab1a3cf6d2675b222f81476
Alex Jackson <45179933+ajaxbits@users.noreply.github.com>
2021-12-14 15:29:53 -0600

    package up the website!

--- flake.nix
+++ flake.nix
@@ -4,30 +4,44 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    gitignore.url = "github:hercules-ci/gitignore.nix";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, gitignore }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
         nodeEnv = pkgs.callPackage ./default.nix { };
         revision = "${self.lastModifiedDate}-${self.shortRev or "dirty"}";
+        inherit (gitignore.lib) gitignoreSource;
       in rec {
-        # packages = {
-        #   ociImage = pkgs.dockerTools.buildLayeredImage {
-        #     name = "gracebobber.com";
-        #     contents = [ pkgs.python3Minimal ];
-        #     tag = revision;
-        #     config = {
-        #       Cmd = [
-        #         "${pkgs.python3Minimal}/bin/python"
-        #         "-m"
-        #         "http.server"
-        #         "8000"
-        #       ];
-        #     };
-        #   };
-        # };
+        packages = with pkgs; {
+          gracebobberdotcom = stdenv.mkDerivation {
+            name = "gracebobberdotcom";
+            src = gitignoreSource ./.;
+            buildPhase = ''
+              ${pkgs.exiftool}/bin/exiftool -Title="Grace Bobber Resume" ./images/Grace-Bobber-Resume.pdf
+            '';
+            installPhase = ''
+              mkdir $out
+              cp -r ./* $out/
+            '';
+          };
+
+          ociImage = dockerTools.buildLayeredImage {
+            name = "gracebobber.com";
+            contents = [ pkgs.python3Minimal packages.gracebobberdotcom ];
+            tag = revision;
+            config = {
+              Cmd = [
+                "${pkgs.python3Minimal}/bin/python"
+                "-m"
+                "http.server"
+                "8000"
+              ];
+            };
+          };
+        };
 
         devShell = pkgs.mkShell {
           buildInputs = with pkgs;
