3d0f4308f2b127b7b1a1f1bbc39f08acc1c4c5aa
Alex Jackson <45179933+ajaxbits@users.noreply.github.com>
2021-12-14 13:35:18 -0600

    add flake-utils for all systems to the flake

--- flake.nix
+++ flake.nix
@@ -2,34 +2,34 @@
   description = "Grace Bobber's Website";
 
   inputs = {
-    unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-    stable.url = "github:NixOS/nixpkgs/nixos-20.09";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = inputs:
-    let
-      system = "x86_64-linux";
-      pkgs = inputs.unstable.legacyPackages.${system};
-      nodeEnv = pkgs.callPackage ./default.nix { };
-    in {
-      devShell."${system}" = pkgs.mkShell {
-        buildInputs = with pkgs;
-          with nodePackages; [
-            nodejs
-            node2nix
-            nodeEnv.shell.nodeDependencies
-            imagemagick
-            libwebp
-            jpegoptim
-            optipng
-            svgo
-            fd
-            exiftool
-          ];
-        shellHook = ''
-          export NODE_PATH=${nodeEnv.shell.nodeDependencies}/lib/node_modules
-          exiftool -title="Grace Bobber Resume" images/Grace-Bobber-Resume.pdf
-        '';
-      };
-    };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+	pkgs = nixpkgs.legacyPackages.${system};
+	nodeEnv = pkgs.callPackage ./default.nix { };
+      in rec {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; with nodePackages; [
+	      nodejs
+	      node2nix
+	      nodeEnv.shell.nodeDependencies
+	      imagemagick
+	      libwebp
+	      jpegoptim
+	      optipng
+	      svgo
+	      fd
+	      exiftool
+	    ];
+	  shellHook = ''
+	    export NODE_PATH=${nodeEnv.shell.nodeDependencies}/lib/node_modules
+	    exiftool -title="Grace Bobber Resume" images/Grace-Bobber-Resume.pdf
+	  '';
+	};
+      }
+    );
 }
