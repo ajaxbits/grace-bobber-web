4f3b2e105958016e398f6534c521499450d0fa88
Alex Jackson <contact@ajaxbits.com>
2022-11-12 13:40:53 -0600

    add ffmpeg

--- flake.nix
+++ flake.nix
@@ -12,84 +12,87 @@
     rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, flake-utils, gitignore, rust-overlay }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        overlays = [ (import rust-overlay) ];
-        pkgs = import nixpkgs {
-          inherit system overlays;
-        };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    gitignore,
+    rust-overlay,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      overlays = [(import rust-overlay)];
+      pkgs = import nixpkgs {
+        inherit system overlays;
+      };
 
-        revision = "${self.lastModifiedDate}-${self.shortRev or "dirty"}";
-        inherit (gitignore.lib) gitignoreSource;
+      revision = "${self.lastModifiedDate}-${self.shortRev or "dirty"}";
+      inherit (gitignore.lib) gitignoreSource;
 
-        htmlSource = pkgs.stdenv.mkDerivation {
-          name = "gracebobberdotcom";
-          src = gitignoreSource ./.;
-          preferLocalBuild = true;
-          buildInputs = [ pkgs.exiftool ];
-          buildPhase = ''
-            exiftool -Title="Grace Bobber Resume" -q -P -overwrite_original_in_place ./images/Grace-Bobber-Resume.pdf
-          '';
-          installPhase = ''
-            mkdir $out
-            cp -r ./* $out/
-          '';
-        };
-
-        images_folder = "images";
-        dist_images_folder = "dist/images";
-        thumbnails_folder = "thumbnails";
-
-        optimizejpg = pkgs.writeShellScriptBin "optimizejpg" ''
-          for img in $(${pkgs.fd}/bin/fd -e jpg -e jpeg . ${images_folder});
-          do
-                  ${pkgs.jpegoptim}/bin/jpegoptim --dest="${dist_images_folder}" \
-                          --overwrite \
-                          --all-progressive \
-                          --strip-all \
-                          --max=90 \
-                          --force \
-                          $img
-                  ${pkgs.jpegoptim}/bin/jpegoptim --dest=${thumbnails_folder} \
-                          --overwrite \
-                          --all-progressive \
-                          --strip-all \
-                          --size=10% \
-                          --force \
-                          $img
-          done
+      htmlSource = pkgs.stdenv.mkDerivation {
+        name = "gracebobberdotcom";
+        src = gitignoreSource ./.;
+        preferLocalBuild = true;
+        buildInputs = [pkgs.exiftool];
+        buildPhase = ''
+          exiftool -Title="Grace Bobber Resume" -q -P -overwrite_original_in_place ./images/Grace-Bobber-Resume.pdf
         '';
-
-        optimizepng = pkgs.writeShellScriptBin "optimizepng" ''
-          for img in $( ${pkgs.fd}/bin/fd -e png . ${images_folder} );
-          do
-                  ${pkgs.optipng}/bin/optipng $img -dir ${dist_images_folder} -clobber
-                  ${pkgs.optipng}/bin/optipng $img -dir ${thumbnails_folder} -o 7 -clobber
-          done
+        installPhase = ''
+          mkdir $out
+          cp -r ./* $out/
         '';
+      };
 
+      images_folder = "images";
+      dist_images_folder = "dist/images";
+      thumbnails_folder = "thumbnails";
 
-      in
-      rec {
-        devShell = pkgs.mkShell {
-          buildInputs = with pkgs;
-            with nodePackages; [
-              nodejs
-              fd
-              exiftool
-              nodePackages.prettier
-              netlify-cli
+      optimizejpg = pkgs.writeShellScriptBin "optimizejpg" ''
+        for img in $(${pkgs.fd}/bin/fd -e jpg -e jpeg . ${images_folder});
+        do
+                ${pkgs.jpegoptim}/bin/jpegoptim --dest="${dist_images_folder}" \
+                        --overwrite \
+                        --all-progressive \
+                        --strip-all \
+                        --max=90 \
+                        --force \
+                        $img
+                ${pkgs.jpegoptim}/bin/jpegoptim --dest=${thumbnails_folder} \
+                        --overwrite \
+                        --all-progressive \
+                        --strip-all \
+                        --size=10% \
+                        --force \
+                        $img
+        done
+      '';
 
-              optimizepng
-              optimizejpg
+      optimizepng = pkgs.writeShellScriptBin "optimizepng" ''
+        for img in $( ${pkgs.fd}/bin/fd -e png . ${images_folder} );
+        do
+                ${pkgs.optipng}/bin/optipng $img -dir ${dist_images_folder} -clobber
+                ${pkgs.optipng}/bin/optipng $img -dir ${thumbnails_folder} -o 7 -clobber
+        done
+      '';
+    in rec {
+      devShell = pkgs.mkShell {
+        buildInputs = with pkgs;
+        with nodePackages; [
+          nodejs
+          fd
+          exiftool
+          nodePackages.prettier
+          netlify-cli
+          ffmpeg
 
-              rust-bin.stable."1.58.0".default
-              openssl
-              pkgconfig
-              cargo-edit
-              cargo-watch
-            ];
-        };
-      });
+          optimizepng
+          optimizejpg
+
+          rust-bin.stable."1.58.0".default
+          openssl
+          pkgconfig
+          cargo-edit
+          cargo-watch
+        ];
+      };
+    });
 }
