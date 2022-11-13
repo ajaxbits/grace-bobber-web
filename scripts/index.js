b5cc56df156a3fcf4548f6406c75c96b1db2a1c2
Alex Jackson <contact@ajaxbits.com>
2022-11-12 18:42:01 -0600

    finsh video update

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,18 @@
 // fix the video height on android screens
 let vh = window.innerHeight * 0.01;
-document.documentElement.style.setProperty("--vh", `${vh}px`);
+document.documentElement.style.setProperty('--vh', `${vh}px`);
 // We listen to the resize event
-window.addEventListener("resize", () => {
+window.addEventListener('resize', () => {
   // We execute the same script as before
   let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty("--vh", `${vh}px`);
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
 });
 
 // handle mute button functionality
 const muteButton = document.querySelector(`.mutebutton`);
 const video = document.querySelector(`.video`);
 
-const handleClick = (e) => {
+const handleClick = e => {
   video.muted ? (video.muted = false) : (video.muted = true);
   muteButton.style.removeProperty(`animation`);
 };
--- video-converter.fish
+++ video-converter.fish
@@ -1,14 +1,14 @@
 ffmpeg -i videos/test-video.mp4 \
-    -filter:v "vflip,crop=720:720:525:0,unsharp=5:5:1.0" \
+    -filter:v "hflip,crop=720:720:40:0,unsharp=5:5:1.0" \
     -c:v libvpx-vp9 \
     -crf 40 \
     -deadline best \
     videos/new-splash.webm
 
 ffmpeg -i videos/test-video.mp4 \
-    -filter:v "vflip,crop=720:720:525:0,unsharp=5:5:1.0" \
+    -filter:v "hflip,crop=720:720:40:0,unsharp=5:5:1.0" \
     -c:v libx265 \
-    -crf 32 \
+    -crf 24 \
     -preset veryslow \
     -movflags faststart \
     -tag:v hvc1 \
